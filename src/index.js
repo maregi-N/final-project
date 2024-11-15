87bc399afd2637cbf15b77c906306a984ba6aa8b
maregi-N <maregij@gmail.com>
2024-11-15 12:52:45 +0300

    Updated js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.Value;
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchForElement = document.querySelector("#search-form");
