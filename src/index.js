6ed63d914e1a90c7d59ef4ef685a5295160774db
maregi-N <maregij@gmail.com>
2024-11-25 11:31:41 +0300

    correction in js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = math.round(temperature);
+  temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
       class="weather-app-icon" />`;
 }
