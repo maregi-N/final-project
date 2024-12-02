c68129e372afbb5f21febdf72c676f0f58fa115a
maregi-N <maregij@gmail.com>
2024-12-02 11:31:44 +0300

    Inject forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -51,6 +51,31 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date"> ${day}</div>
+    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong> 15°c </strong>
+      </div>
+      <div class="weather-forecast-temperature">7°c</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchForElement = document.querySelector("#search-form");
 searchForElement.addEventListener("submit", handleSearchSubmit);
+displayForecast();
